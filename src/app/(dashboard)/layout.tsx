49c646937128f5cedbde46407653fe570385e0f2
yuragms <yura.gms@gmail.com>
2024-12-15 17:13:41 +0200

    145

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,31 @@
+import { AppSidebar } from '@/components/app-sidebar';
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar';
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <div>
+          {' '}
+          <SidebarTrigger className="-ml-1" />
+        </div>
+        <main
+          suppressHydrationWarning
+          className="flex flex-1 flex-col gap-4 p-4 pt-0"
+        >
+          {children}
+        </main>
+      </SidebarInset>
+      ;
+    </SidebarProvider>
+  );
+}
