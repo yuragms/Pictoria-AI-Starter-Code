e6e9ea5917d3da8b70d238e50d1432572adee0ee
yuragms <yura.gms@gmail.com>
2025-01-01 19:23:58 +0200

    next

--- src/app/(dashboard)/image-generation/page.tsx
+++ src/app/(dashboard)/image-generation/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const ImageGeneration = () => {
-  return <div>ImageGeneration Page</div>;
+  return (
+    <section className="container mx-auto grid gap-4 grid-cols-3 overflow-hidden">
+      Form
+      <div className="col-span-2 rounded">Output Images</div>
+    </section>
+  );
 };
 
 export default ImageGeneration;
