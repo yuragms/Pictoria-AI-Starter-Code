319b3a3d5fbe77ca599160266924d13c5a5f5ef2
yuragms <yura.gms@gmail.com>
2025-01-06 21:56:04 +0200

    my-images-41420

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'replicate.delivery',
       },
+      {
+        protocol: 'https',
+        hostname: 'rhqlqeyxwdcguykdekhd.supabase.co',
+      },
     ],
   },
 };
--- src/components/gallery/GalleryComponent.tsx
+++ src/components/gallery/GalleryComponent.tsx
@@ -1,4 +1,5 @@
 import { Tables } from '@datatypes.types';
+import Image from 'next/image';
 import React from 'react';
 
 type Imageprops = {
@@ -11,7 +12,32 @@ interface GalleryProps {
 
 const GalleryComponent = ({ images }: GalleryProps) => {
   console.log(images);
-  return <div>GalleryComponent</div>;
+  if (images.length === 0) {
+    return (
+      <div className="flex items-center justify-center h-[50vh] text-muted-foreground">
+        No Images found
+      </div>
+    );
+  }
+  return (
+    <section className="container mx-auto py-8">
+      <div className="columns-4 gap-4 space-y-4">
+        {images.map((image, index) => {
+          <div key={index}>
+            <div className="relative overflow-hidden cursor-pointer transition-transform">
+              <Image
+                src={image.url}
+                alt={image.promt}
+                width={image.width}
+                height={image.height}
+                className="object-cover rounded"
+              />
+            </div>
+          </div>;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default GalleryComponent;
