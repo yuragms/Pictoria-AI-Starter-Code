b060c6f34ccf128001c7bdc58779876487103b87
yuragms <yura.gms@gmail.com>
2025-01-11 16:39:44 +0200

    Flux model Training

--- src/app/actions/image-actions.ts
+++ src/app/actions/image-actions.ts
@@ -189,3 +189,38 @@ export async function getImages(limit?: number) {
     data: imageWithUrls || null,
   };
 }
+
+export async function deleteImage(id: string, imageName: string) {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) {
+    return {
+      error: 'Unauthorized',
+      success: false,
+      data: null,
+    };
+  }
+  const { data, error } = await supabase
+    .from('generated_images')
+    .delete()
+    .eq('id', id);
+  if (error) {
+    return {
+      error: error.message || 'Failed to delete image',
+      success: false,
+      data: null,
+    };
+  }
+  await supabase.storage
+    .from('generated_images')
+    .remove([`${user.id}/${imageName}`]);
+
+  return {
+    error: null,
+    success: true,
+    data: data,
+  };
+}
--- src/components/gallery/ImageDialog.tsx
+++ src/components/gallery/ImageDialog.tsx
@@ -8,7 +8,10 @@ import {
 } from '@/components/ui/sheet';
 import Image from 'next/image';
 import { Button } from '../ui/button';
-import { Download, Trash2 } from 'lucide-react';
+import { Download } from 'lucide-react';
+import { Badge } from '../ui/badge';
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
+import DeleteImage from './DeleteImage';
 
 interface ImageDialogProps {
   image: { url: string | undefined } & Tables<'generated_images'>;
@@ -16,29 +19,126 @@ interface ImageDialogProps {
 }
 
 const ImageDialog = ({ image, onClose }: ImageDialogProps) => {
+  const handleDownload = () => {
+    fetch(image.url || '').then((response) => {
+      response
+        .blob()
+        .then((blob) => {
+          const url = window.URL.createObjectURL(new Blob([blob]));
+          const link = document.createElement('a');
+          link.href = url;
+          link.setAttribute(
+            'download',
+            `generated-image-${Date.now()}.${image?.output_format}`
+          );
+          document.body.appendChild(link);
+          link.click();
+          //cleanup
+          link.parentNode?.removeChild(link);
+        })
+        .catch((error) => console.log(error));
+    });
+  };
   return (
     <Sheet open={true} onOpenChange={onClose}>
-      <SheetContent>
+      <SheetContent className="max-w-full sm:max-w-xl w-full">
         <SheetHeader>
-          <SheetTitle>Are you absolutely sure?</SheetTitle>
-          <div className="relative w-fit h-fit">
-            <Image
-              src={image.url || ''}
-              alt={image.prompt || ''}
-              width={image.width || 0}
-              height={image.height || 0}
-              className="w-full h-auto flex mb-3 rounded"
-            />
-            <div className="flex gap-4 absolute bottom-4 right-4">
-              <Button className="w-fit">
-                <Download className="w-4 h-4 mr-2" />
-                Download
-              </Button>
-              <Button className="w-fit" variant={'destructive'}>
-                <Trash2 className="w-4 h-4" />
-              </Button>
+          <SheetTitle className="text-2xl w-full">Image Details</SheetTitle>
+          <ScrollArea className="flex flex-col h-[100vh]">
+            <div className="relative w-fit h-fit">
+              <Image
+                src={image.url || ''}
+                alt={image.prompt || ''}
+                width={image.width || 0}
+                height={image.height || 0}
+                className="w-full h-auto flex mb-3 rounded"
+              />
+              <div className="flex gap-4 absolute bottom-4 right-4">
+                <Button className="w-fit" onClick={handleDownload}>
+                  <Download className="w-4 h-4 mr-2" />
+                  Download
+                </Button>
+                <DeleteImage
+                  imageId={image.id.toString()}
+                  onDelete={onClose}
+                  className="w-fit"
+                  imageName={image.image_name}
+                />
+              </div>
             </div>
-          </div>
+            <hr className="inline-block w-full border-primary/30 mb-2" />
+            <p className="text-primary/90 w-full flex flex-col ">
+              <span className="text-primary text-xl font-semibold">Prompt</span>
+              {image.prompt}
+            </p>
+            <hr className="inline-block w-full border-primary/30 mb-3" />
+            <div className="flex flex-wrap gap-3 mb-32">
+              <Badge
+                variant={'secondary'}
+                className="rounded-full border border-primary/30 px-4 py-2 text-sm font-normal"
+              >
+                <span className="text-primary uppercase mr-2 font-semibold">
+                  Model ID:
+                </span>
+                {image.model}
+              </Badge>
+              <Badge
+                variant={'secondary'}
+                className="rounded-full border border-primary/30 px-4 py-2 text-sm font-normal"
+              >
+                <span className="text-primary uppercase mr-2 font-semibold">
+                  Aspect Ratio
+                </span>
+                {image.aspect_ratio}
+              </Badge>
+              <Badge
+                variant={'secondary'}
+                className="rounded-full border border-primary/30 px-4 py-2 text-sm font-normal"
+              >
+                <span className="text-primary uppercase mr-2 font-semibold">
+                  Dimensions:
+                </span>
+                {image.width} x {image.height}
+              </Badge>
+              <Badge
+                variant={'secondary'}
+                className="rounded-full border border-primary/30 px-4 py-2 text-sm font-normal"
+              >
+                <span className="text-primary uppercase mr-2 font-semibold">
+                  Guidance:
+                </span>
+                {image.guidance}
+              </Badge>
+              <Badge
+                variant={'secondary'}
+                className="rounded-full border border-primary/30 px-4 py-2 text-sm font-normal"
+              >
+                <span className="text-primary uppercase mr-2 font-semibold">
+                  Interence Steps
+                </span>
+                {image.num_inference_steps}
+              </Badge>
+              <Badge
+                variant={'secondary'}
+                className="rounded-full border border-primary/30 px-4 py-2 text-sm font-normal"
+              >
+                <span className="text-primary uppercase mr-2 font-semibold">
+                  Output Format
+                </span>
+                {image.output_format}
+              </Badge>
+              <Badge
+                variant={'secondary'}
+                className="rounded-full border border-primary/30 px-4 py-2 text-sm font-normal"
+              >
+                <span className="text-primary uppercase mr-2 font-semibold">
+                  Created At:
+                </span>
+                {new Date(image.created_at).toLocaleDateString()}
+              </Badge>
+            </div>
+            <ScrollBar orientation="vertical" />
+          </ScrollArea>
         </SheetHeader>
       </SheetContent>
     </Sheet>
