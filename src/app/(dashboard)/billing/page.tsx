55c2e027254e808285023e7b1f7a86e7aba80748
yuragms <yura.gms@gmail.com>
2025-01-17 17:47:37 +0200

    stripe750

--- src/app/(dashboard)/billing/page.tsx
+++ src/app/(dashboard)/billing/page.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const BillingPage = () => {
-  return <div>BillingPage</div>;
+  return (
+    <section className="container mx-auto space-y-8">
+      <div>
+        <h1 className="text-3xl font-bold tracking-tight">Plans & Billing</h1>
+        <p className="text-muted-foreground">
+          Manage your subscription and billing information
+        </p>
+      </div>
+      BillingPage
+    </section>
+  );
 };
 
 export default BillingPage;
---
+++ src/lib/stripe/config.ts
@@ -0,0 +1,13 @@
+import Stripe from 'stripe';
+
+export const stripe = new Stripe(
+  process.env.STRIPE_SECRET_KEY_LIVE ?? process.env.STRIPE_SECRET_KEY ?? '',
+  {
+    // https://github.com/stripe/stripe-node#configuration
+    // https://stripe.com/docs/api/versioning
+    // @ts-ignore
+    // apiVersion: null,
+    // Register this as an official Stripe plugin.
+    // https://stripe.com/docs/building-plugins#setappinfo
+  }
+);
