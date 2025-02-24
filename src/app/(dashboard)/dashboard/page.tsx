b84621b4e6abbca26d9c7556cfc729b9e0d8ece1
yuragms <yura.gms@gmail.com>
2025-02-24 20:21:02 +0200

    Creating Dashboard page112022

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,6 +1,6 @@
 export default function Page() {
   return (
-    <div>
+    <section>
       {/* <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
         <div className="grid auto-rows-min gap-4 md:grid-cols-3">
           <div className="aspect-video rounded-xl bg-muted/50" />
@@ -10,6 +10,6 @@ export default function Page() {
         <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
       </div> */}
       Dashboard
-    </div>
+    </section>
   );
 }
