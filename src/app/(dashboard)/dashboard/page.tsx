b2a22baebc4f0a6177e51fe2a3c73107de814c87
yuragms <yura.gms@gmail.com>
2025-03-05 21:26:06 +0200

    creating-dashboard-page113009

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,6 +1,33 @@
-export default function Page() {
+import { getCredits } from '@/app/actions/credit-actions';
+import { getImages } from '@/app/actions/image-actions';
+import { fetchModels } from '@/app/actions/model-actions';
+import StatsCards from '@/components/dashboard/StatsCards';
+import { createClient } from '@/lib/supabase/server';
+
+export default async function Page() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const { data: models, count: modelCount } = await fetchModels();
+
+  const { data: credits } = await getCredits();
+  const { data: images } = await getImages();
+
+  const imageCount = images?.length || 0;
   return (
-    <section>
+    <section className="container mx-auto flex-1 space-y-6">
+      <div className="flex items-center justify-between">
+        <h2 className="text-3xl font-bold tracking-tight">
+          Welcome back, {user?.user_metadata.full_name}
+        </h2>
+      </div>
+      <StatsCards
+        imageCount={imageCount}
+        modelCount={modelCount}
+        credits={credits}
+      />
       {/* <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
         <div className="grid auto-rows-min gap-4 md:grid-cols-3">
           <div className="aspect-video rounded-xl bg-muted/50" />
@@ -9,7 +36,6 @@ export default function Page() {
         </div>
         <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
       </div> */}
-      Dashboard
     </section>
   );
 }
---
+++ src/components/dashboard/StatsCards.tsx
@@ -0,0 +1,73 @@
+import { Database } from '@datatypes.types';
+import React from 'react';
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
+import { ImageIcon, LayersIcon, Wallet, ZapIcon } from 'lucide-react';
+
+interface StatsCardsProps {
+  imageCount: number;
+  modelCount: number;
+  credits: Database['public']['Tables']['credits']['Row'] | null;
+}
+
+const StatsCards = ({ imageCount, modelCount, credits }: StatsCardsProps) => {
+  return (
+    <div className="grid grid-cols-4 gap-6">
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-sm font-medium">Total Images</CardTitle>
+          <ImageIcon className="h-4 w-4 text-muted-foreground" />
+        </CardHeader>
+        <CardContent>
+          <div className="text-2xl font-bold">{imageCount}</div>
+          <p className="text-xs text-muted-foreground">
+            Images generated so far
+          </p>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-sm font-medium">Total Models</CardTitle>
+          <LayersIcon className="h-4 w-4 text-muted-foreground" />
+        </CardHeader>
+        <CardContent>
+          <div className="text-2xl font-bold">{modelCount}</div>
+          <p className="text-xs text-muted-foreground">Custom models trained</p>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-sm font-medium">Image Credits</CardTitle>
+          <ZapIcon className="h-4 w-4 text-muted-foreground" />
+        </CardHeader>
+        <CardContent>
+          <div className="text-2xl font-bold">
+            {credits?.image_generation_count || 0}/
+            {credits?.max_image_generation_count || 0}
+          </div>
+          <p className="text-xs text-muted-foreground">
+            Available generation credits
+          </p>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-sm font-medium">
+            Training Credits
+          </CardTitle>
+          <Wallet className="h-4 w-4 text-muted-foreground" />
+        </CardHeader>
+        <CardContent>
+          <div className="text-2xl font-bold">
+            {credits?.model_training_count || 0}/
+            {credits?.max_model_training_count || 0}
+          </div>
+          <p className="text-xs text-muted-foreground">
+            Available traning credits
+          </p>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default StatsCards;
